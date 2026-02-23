a4c73c49670741067d235f1e7ab4db46f0ded7bc
Seba-RC <sebarc2005@gmail.com>
2026-02-23 12:40:05 -0300

    Containerized application

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18-alpine
+WORKDIR /app
+
+ENV HOST 0.0.0.0
+
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE 8080
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Serve your assets and js files
+app.use(express.static(path.join(__dirname, '/')));
+
+// Send index.html when someone visits the site
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Goober is live on port ${port}`);
+});
