6c0d3e3b6a4a6fad4fbfe97a32d63c3ae66d1026
Seba-RC <sebarc2005@gmail.com>
2026-02-23 01:53:52 -0300

    added dash and double jump!

--- game.js
+++ game.js
@@ -26,6 +26,10 @@ let remotePlayer;
 
 let cursors;
 
+var jumpCounter = 0; // variable to track the number of jumps
+
+var dashCounter = 0; // variable to track the number of dashes
+
 function preload() {
     // load assets!
     //map
@@ -56,7 +60,8 @@ function create() {
     const map = this.make.tilemap({ key: 'map' });
     console.log("Map Loaded! Layers found:", map.layers.map(l => l.name));
     
-
+    // you might ask Seba why are there two dead grass tiles
+    // tiled did a funny move
     const platformTiles = map.addTilesetImage('platform', 'grass_img');
     const dirtTiles = map.addTilesetImage('dirt', 'dirt_img');
     const deadgrassTiles = map.addTilesetImage('deadgrass', 'dead_img');
@@ -68,35 +73,71 @@ function create() {
     const layer = map.createLayer('Tile Layer 1', allTiles, 0, 0);
 
     
-    layer.setCollisionBetween(1, 1000);
+    layer.setCollisionBetween(1, 1000); // sets collision between all tiles 
 
-    player = this.physics.add.sprite(5400, 7000, 'goober').setScale(0.2); 
+    player = this.physics.add.sprite(5400, 7000, 'goober').setScale(0.2); // hard coded coordinates Yes I Do Know that there is a better way but still.
 
-    this.physics.add.collider(player, layer);
+    this.physics.add.collider(player, layer); // adds collision with the player
 
-    this.cameras.main.startFollow(player, true, 0.1, 0.1);
+    this.cameras.main.startFollow(player, true, 0.1, 0.1); // makes camera follow the player
 
-    this.cameras.main.setBounds(0, 0, 30000, 30000);
+    this.cameras.main.setBounds(0, 0, 30000, 30000); // sets camera bounds (being a little extra)
 
-    cursors = this.input.keyboard.createCursorKeys();
+    cursors = this.input.keyboard.createCursorKeys(); // sets up input 
 
     }
 
 function update() {
 
+    // todo: when I add the second player, make the update function track the *coordinates* of the second player
+
+    
+
     if (!player) return;
 
     if (cursors.left.isDown) {
-        player.setVelocityX(-200);
+        player.setVelocityX(-250);
         player.setFlipX(true);
     } else if (cursors.right.isDown) {
-        player.setVelocityX(200);
+        player.setVelocityX(250);
         player.setFlipX(false);
     } else {
         player.setVelocityX(0);
     }
 
-    if (cursors.up.isDown && player.body.blocked.down) {
-        player.setVelocityY(-400);
+    if (cursors.up.isDown && Phaser.Input.Keyboard.JustDown(cursors.up) && jumpCounter < 1) {
+        player.setVelocityY(-330);
+        jumpCounter++;
     }
+    if (player.body.blocked.down) {
+        jumpCounter = 0;
+        dashCounter = 0;
+    }
+
+    if (cursors.shift.isDown) {
+        if (dashCounter < 1) {
+        
+        if (cursors.up.isDown && (cursors.right.isDown || cursors.left.isDown)) {
+        
+        player.setVelocityX(player.body.velocity.x * 2.5);
+        player.setVelocityY(-400);
+        this.time.delayedCall(200, () => {
+            player.setVelocityX(100);
+            player.setVelocityY(0);
+            dashCounter++;
+        });
+        } else if (cursors.right.isDown || cursors.left.isDown) {
+        player.setVelocityX(player.body.velocity.x * 2.5);
+        this.time.delayedCall(200, () => {
+            player.setVelocityX(100);
+            dashCounter++;
+        });
+    }
+        }
+    }
+
+    this.input.keyboard.on('keydown-R', function () {
+        player.setPosition(5400, 7000); // resets player position to the hard coded coordinates
+    });
 }
+
